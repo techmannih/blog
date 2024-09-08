98377472975a16f330d77bfab15ce8456b88d769
techmannih <manishchaudharyttt@gmail.com>
2024-09-08 08:47:14 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import postRoutes from "./routes/post.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors(
+  {
+    origin: "http://localhost:3000",
+  }
+));
 app.use(express.json());
 app.use("/api", postRoutes);
 app.get("/", (req, res) => {
